8bccc4a2e6fc9b6829bc1d3efa2a981f7fb6e464
Parth11128 <parthsaxena11128@gmail.com>
2026-02-14 17:25:29 +0530

    Create As1Q2.c

---
+++ As1Q2.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int n, i, sum = 0;
+    float avg;
+    int *arr;
+
+    printf("Enter size: ");
+    scanf("%d", &n);
+
+    arr = malloc(n * sizeof(int));
+
+    printf("Enter elements:\n");
+    for(i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+        sum += arr[i];
+    }
+
+    printf("Elements: ");
+    for(i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    avg = (float)sum / n;
+
+    printf("\nSum = %d", sum);
+    printf("\nAverage = %.2f", avg);
+
+    free(arr);
+
+    return 0;
+}
